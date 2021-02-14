a0287a0ed3f5c3b41039d9440a815a40f1b52887
Andy Wendt <andy@awendt.com>
2021-02-14 09:29:35 -0700

    Update bash

--- init.sh
+++ init.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# install ansible ppa
+sudo apt-add-repository ppa:ansible/ansible -y
+
+# install stuff
+sudo apt update
+sudo apt install -y aptitude ansible dialog
+
 ansible-playbook ./linux-playbook.yml
 
 echo "Linux setup"
--- keymappings.sh
+++ keymappings.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# http://homeowmorphism.com/articles/17/Remap-CapsLock-Backspace-Sierra
-# https://developer.apple.com/library/archive/technotes/tn2450/_index.html#//apple_ref/doc/uid/DTS40017618-CH1-KEY_TABLE_USAGES
 
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000029}]}'
